c5c89f846dfe9cef28daf0614fd76f641c0a27f2
rtyke <olga.goi@gmail.com>
2018-12-18 15:20:17 +0300

    [refactor] add verifications

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from typing import Dict, List
-
 import requests
-import sys
-import json
-import time
 import os
 
 SECRET_KEY = os.environ['KEY']
@@ -13,46 +9,39 @@ HEADERS = {'X-Api-App-Id': SECRET_KEY}
 
 def request_page_with_vacancies(since_date: int, page_number: int):
     vacancies_url = 'https://api.superjob.ru/2.0/vacancies/'
-    params = {'count': 100, 'page': page_number, 'date_published_from': since_date}
-    response = requests.get(vacancies_url, headers=HEADERS, params=params)
-    if response.status_code != 200:
-        return False
-    else:
+    params = {
+        'count': 100,
+        'page': page_number,
+        'date_published_from': since_date
+    }
+    try:
+        response = requests.get(vacancies_url, headers=HEADERS, params=params)
         return response
+    except requests.RequestException:
+        print('Connection error')
+        return False
 
 
-def extract_vacancies_from_response_data(response_data) -> List[Dict]:
-    vacancies_attrs = response_data.json()['objects']
-    return vacancies_attrs
-
-
-def scrape_vacancies(since_date):
+def scrape_vacancies(since_date: int) -> List[Dict]:
+    vacancies_all = []
     for page_number in range(6):
-        vacancies_response = request_page_with_vacancies(
+        response = request_page_with_vacancies(
             since_date=since_date,
             page_number=page_number,
         )
-        if not vacancies_response:
-            raise Exception('Failed connection')
-        yield extract_vacancies_from_response_data(vacancies_response)
-
-
-def save_to_json(data: List[Dict]) -> None:
-    json_name = '{}.json'.format(time.time())
-    with open(json_name, 'w') as fo:
-        json.dump(data, fo, ensure_ascii=False)
+        if response:
+            vacancies_on_page = response.json().get('objects', [])
+            vacancies_all.extend(vacancies_on_page)
+    return vacancies_all
 
 
 def main():
-    try:
-        for vacancies_page in scrape_vacancies(since_date=1544630400):
-            if vacancies_page:
-                save_to_json(vacancies_page)
-            else:
-                # TODO get page number. Does generator have attributes?
-                print(f'No content on page')
-    except Exception:
-        sys.exit('Failed connection')
+    vacancies_all = scrape_vacancies(since_date=1544630400)
+    if not vacancies_all:
+        print('No vacancies for date 1544630400')
+    else:
+        for vacancy in vacancies_all:
+            print(vacancy)
 
 
 if __name__ == '__main__':
