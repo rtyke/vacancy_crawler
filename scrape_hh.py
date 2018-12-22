96446831836fb791f7ef69bbabc97942d1de5561
ggoofie <farsiyan.vartan@yandex.ru>
2018-12-22 11:23:09 +0300

    monthly generator vacancies

--- scrape_hh.py
+++ scrape_hh.py
@@ -1,19 +1,37 @@
 import requests
 import json
 from typing import Dict
+from datetime import date, datetime, timedelta
 
 
 URL = 'https://api.hh.ru/vacancies/'
 HEADERS = {'User-Agent': 'api-test-agent'}
 
 
-def get_hh_vacancies() -> Dict:
+def get_datetime_month_ago():
+    today = datetime.now()
+    current_day = today.day
+    last_month = (today.replace(day=1) -
+                  timedelta(days=1)).replace(day=current_day)
+    return last_month
+
+
+def generator_hh_vacancies_last_month() -> Dict:
     global URL, HEADERS
-    params = {'page': 0, 'per_page': 100, 'period': 30}
-    while params['page'] < 20:
-        response = requests.get(url=URL, params=params, headers=HEADERS)
-        params['page'] += 1
-        yield from response.json()['items']
+    start_time = get_datetime_month_ago()
+    end_time = datetime.now()
+    while start_time < end_time:
+        interval_end = start_time + timedelta(days=1)
+        params = {'page': 0,
+                  'per_page': 100,
+                  'date_from': datetime.strftime(start_time, "%Y-%m-%dT%H:%M:%S"),
+                  'date_to': datetime.strftime(interval_end, "%Y-%m-%dT%H:%M:%S")}
+        # print(start_time, interval_end, sep=' ======> ')
+        while params['page'] < 20:
+            response = requests.get(url=URL, params=params, headers=HEADERS)
+            params['page'] += 1
+            yield from response.json()['items']
+        start_time = interval_end + timedelta(minutes=5)
 
 
 def pretty_print_json(data: Dict):
@@ -21,9 +39,10 @@ def pretty_print_json(data: Dict):
 
 
 def main():
-    for vacancy in get_hh_vacancies():
-        pretty_print_json(vacancy)
-        # break
+    result = []
+    for vacancy in generator_hh_vacancies_last_month():
+        result.append(vacancy['id'])
+    print(len(result), len(set(result)))
 
 
 if __name__ == '__main__':
