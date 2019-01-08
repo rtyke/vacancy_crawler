e382688c8437fe82bb6408bf0581b9b904674e38
rtyke <olga.goi@gmail.com>
2019-01-09 00:27:58 +0300

    [add] add class for Salary

--- data_handling_orm.py
+++ data_handling_orm.py
@@ -1,7 +1,7 @@
 import json
 import time
 
-from orm import Vacancy, Session, engine, Base
+from orm import Vacancy, Salary, Session, engine, Base
 from scrape_superjob import get_job_description
 
 
@@ -16,7 +16,7 @@ def get_newest_timestamp(session):
 
 def put_vacancy_to_db(session, vacancy):
     vacancy_orm = Vacancy(
-        vacancy_id=vacancy['id'],
+        site_id=vacancy['id'],
         title=vacancy['profession'],
         unixtime=vacancy['date_published'],
         description=get_job_description(vacancy),
@@ -24,12 +24,21 @@ def put_vacancy_to_db(session, vacancy):
         metro=json.dumps(vacancy['metro'], ensure_ascii=False),
         type_of_work=vacancy['type_of_work']['title'],
         experience=vacancy['experience']['title'],
-        salary=1000000,  # TODO discuss this
+        # salary=1000000,  # TODO discuss this
         specializations=json.dumps(vacancy['catalogues'], ensure_ascii=False),  # TODO discuss this
         is_archive=vacancy['is_archive'],
         update_time=int(time.time()),
     )
+    salary_orm = Salary(
+        agreement=vacancy['agreement'],
+        payment_from=vacancy['payment_from'],
+        payment_to=vacancy['payment_to'],
+        currency=vacancy['currency'],
+        vacancy=vacancy_orm
+
+    )
     session.add(vacancy_orm)
+    session.add(salary_orm)
     session.commit()
 
 
--- orm.py
+++ orm.py
@@ -1,19 +1,18 @@
-import json
-
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship, backref
 
 Base = declarative_base()
-engine = create_engine('sqlite:///vacancies_db.db', echo=True)
+# engine = create_engine('sqlite:///vacancies_db.db', echo=True)
+engine = create_engine('sqlite:///new.db', echo=True)
 Session = sessionmaker(bind=engine)
 
 
 class Vacancy(Base):
     __tablename__ = 'vacancies'
-    id_pk = Column(Integer, primary_key=True)
-    hash = None  # how to create?
-    vacancy_id = Column('id', Integer)
+    pk = Column(Integer, primary_key=True)
+    # hash = None  # how to create?
+    site_id = Column('site_id', Integer)
     title = Column('title', String)
     unixtime = Column('unixtime', Integer)
     description = Column('description', String)
@@ -21,15 +20,16 @@ class Vacancy(Base):
     metro = Column('metro', String)
     type_of_work = Column('type_of_work', String)
     experience = Column('experience', String)
-    salary = Column('salary', Integer)
+    # salary = Column('salary', Integer)
+    # salary = relationship('Salary', uselist=False, back_populates='vacancies')
     specializations = Column('specializations', String)
     is_archive = Column('is_archive', Boolean)
     update_time = Column('update_time', Integer)
 
-    def __init__(self, vacancy_id, title, unixtime, description, address, metro,
-                 type_of_work, experience, salary, specializations, is_archive,
+    def __init__(self, site_id, title, unixtime, description, address, metro,
+                 type_of_work, experience, specializations, is_archive,
                  update_time):
-        self.vacancy_id = vacancy_id
+        self.site_id = site_id
         self.title = title
         self.unixtime = unixtime
         self.description = description
@@ -37,7 +37,25 @@ class Vacancy(Base):
         self.metro = metro
         self.type_of_work = type_of_work
         self.experience = experience
-        self.salary = salary
+        # self.salary = salary
         self.specializations = specializations
         self.is_archive = is_archive
         self.update_time = update_time
+
+
+class Salary(Base):
+    __tablename__ = 'salary'
+    pk = Column(Integer, primary_key=True)
+    agreement = Column('agreement', Boolean)
+    payment_from = Column('payment_from', Integer)
+    payment_to = Column('payment_to', Integer)
+    currency = Column('currency', String)
+    vacancy_pk = Column(Integer, ForeignKey('vacancies.pk'))
+    vacancy = relationship(Vacancy, backref=backref('salary', uselist=False))
+
+    def __init__(self, agreement, payment_from, payment_to, currency, vacancy):
+        self.agreement = agreement
+        self.payment_from = payment_from
+        self.payment_to = payment_to
+        self.currency = currency
+        self.vacancy = vacancy
