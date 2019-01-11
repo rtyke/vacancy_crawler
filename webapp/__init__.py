e549567701784290d46347f83d810ca169a199f2
rtyke <olga.goi@gmail.com>
2019-01-11 18:58:04 +0300

    [add] output date in human-readable format

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 from webapp.database import db_session
 from webapp.models import Vacancy
+from webapp.utils import strtime_from_unixtime
 
 
 def create_app():
@@ -15,7 +16,12 @@ def create_app():
     @app.route('/')
     def index():
         page_title = 'Scraped Vacancies'
-        vacancies_last = db_session.query(Vacancy).order_by(Vacancy.published_date.desc())[:10]
-        return render_template('index.html', title=page_title, vacancies=vacancies_last)
+        vacancies_last = db_session.query(Vacancy).order_by(Vacancy.published_date.desc()).limit(10)
+        return render_template(
+            'index.html',
+            title=page_title,
+            vacancies=vacancies_last,
+            convert_unixtime=strtime_from_unixtime,
+        )
 
     return app
