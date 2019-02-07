e0e7b7e0d5b00c7ffb78803971ecf684ea40c50c
rtyke <olga.goi@gmail.com>
2019-02-08 01:26:08 +0300

    [refactor] remove redundant files

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from webapp import create_app
-from webapp.models import init_db
-from webapp.models import fill_in_fields_handbook
-
-app = create_app()
-
-with app.app_context():
-    init_db()
-    fill_in_fields_handbook()
