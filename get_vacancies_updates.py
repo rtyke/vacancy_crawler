16ec7a4ae345d18a3c5e02a3c94993e5e25bad95
rtyke <olga.goi@gmail.com>
2019-02-07 20:44:00 +0300

    [add] update vacancy for sj and hh

--- get_vacancies_updates.py
+++ get_vacancies_updates.py
@@ -3,6 +3,7 @@ from celery.schedules import crontab
 
 from webapp import create_app
 from webapp.gather_resumes_sj import gather_vacancies_sj
+from webapp.update_vacancies_hh import update_vacancies_for_field_hh
 
 
 flask_app = create_app()
@@ -10,19 +11,21 @@ celery_app = Celery('get_vacancies_updates', broker='pyamqp://guest@localhost//'
 
 
 @celery_app.task
-def update_resumes_one_field_sj(job_field):
+def update_vacancies_one_field(job_field_ids):
     with flask_app.app_context():
-        gather_vacancies_sj(run='update', job_field=job_field)
+        update_vacancies_for_field_hh(job_field_ids[0])
+        gather_vacancies_sj(run='update', job_field=job_field_ids[1])
 
 
 @celery_app.task
-def update_resume_all_fields_sj():
-    for sj_job_field_id in [33, 136, 381, 284, 100, 234, 11, 76]:
-        update_resumes_one_field_sj.delay(sj_job_field_id)
+def update_vacancies_all_fields():
+    hh_sj_field_ids = [(5, 381), (3, 234), (6, 76), (2, 11), (19, 284), (23, 100), (1, 33), (13, 136)]
+    for field_id in hh_sj_field_ids:
+        update_vacancies_one_field.delay(field_id)
 
 
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(crontab(minute='*/2'), update_resume_all_fields_sj.s())
+    sender.add_periodic_task(crontab(minute='*/2'), update_vacancies_all_fields.s())
 
 
--- webapp/update_vacancies_hh.py
+++ webapp/update_vacancies_hh.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import exists
+
 from webapp.models import db_session, Vacancy
 from scrape_hh import generator_hh_vacancies
 from convert_hh_to_orm import convert_vacancy_to_orm
-from sqlalchemy import exists
-from filing_db_hh import specialization_id_hh
-from datetime import datetime
+# from filing_db_hh import specialization_id_hh
 
 
 def update_vacancy(specialization_id_hh):
+    # TODO depreciated
     last_time = Vacancy.query.order_by(Vacancy.added_to_db_at.desc()).first().added_to_db_at
     for spec_id in specialization_id_hh:
         for vacancy in generator_hh_vacancies(last_time, datetime.now(), spec_id):
@@ -14,5 +17,8 @@ def update_vacancy(specialization_id_hh):
                 convert_vacancy_to_orm(vacancy, spec_id)
 
 
-if __name__ == '__main__':
-    update_vacancy(specialization_id_hh)
+def update_vacancies_for_field_hh(field_id):
+    last_time = Vacancy.query.order_by(Vacancy.added_to_db_at.desc()).first().added_to_db_at
+    for vacancy in generator_hh_vacancies(last_time, datetime.now(), field_id):
+        if not db_session.query(exists().where(Vacancy.id_on_site == vacancy['id'])).scalar():
+            convert_vacancy_to_orm(vacancy, field_id)
